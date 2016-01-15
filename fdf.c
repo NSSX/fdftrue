b96199d277cfb99324c822fbdc2d5a55d914c7e0
Anthony VELLA <avella@e3r11p13.42.fr>
2016-01-15 14:55:59 +0100

    l

--- fdf.c
+++ fdf.c
@@ -13,6 +13,9 @@
 #include <fcntl.h>
 #include "struct.h"
 
+char ***set_coord2(char ***coord, int i, int x, int y, int zoom);
+char ***set_coord3(char ***coord, int i, int x, int y, int zoom, int rotate);
+
 void draw_line2(void *mlx, void *win, float xi, float yi, float xf, float yf, int color)
 {
   int dx,dy,i,xinc,yinc,cumul,x,y ;
@@ -91,11 +94,35 @@ void fill_iso_rect(void *mlx, void *win, int x, int y, int t,int color)
   draw_line2(mlx, win, x + t, y + t, x, y, color);
 }
 
+void legend(t_struct *param)
+{
+  mlx_string_put(param->mlx, param->win, 0, 0, 0x00FFFF, "Commande Disponible :");
+  mlx_string_put(param->mlx, param->win, 0, 25, 0x00FFFF, "Zoom + : Q  | Zoom - : E");
+  mlx_string_put(param->mlx, param->win, 0, 60, 0x00FFFF, "Deplacement :");
+  mlx_string_put(param->mlx, param->win, 0, 80, 0x00FFFF, "Gauche : A");
+  mlx_string_put(param->mlx, param->win, 0, 100, 0x00FFFF, "Droite : D");
+  mlx_string_put(param->mlx, param->win, 0, 120, 0x00FFFF, "Haut : W");
+  mlx_string_put(param->mlx, param->win, 0, 140, 0x00FFFF, "Bas : S");
+  mlx_string_put(param->mlx, param->win, 0, 160, 0x00FFFF, "Return : R");
+}
+
 int close_mlx(int keycode, t_struct *param)
 {
   static int x;
   static int y;
-
+  static int xd;
+  static int yd;
+  static int rotate;
+  static int r;
+  
+  if(!r)
+    r = 0;
+  if(!rotate)
+    rotate = 0;
+  if(!xd)
+    xd = 300;
+  if(!yd)
+    yd = 60;
   if(!y)
     y = 400;
   if(!x)
@@ -104,30 +131,132 @@ int close_mlx(int keycode, t_struct *param)
     exit(1);
   if(keycode == 2)
     {
-      x+= 40;
-      fill_iso_rect(param->mlx, param->win, x, y, 20,0x00FFFF);
+      xd += 40;
+      mlx_destroy_window(param->mlx, param->win);
+      param->win = mlx_new_window(param->mlx, 900, 900, "42");
+      legend(param);
+      if(param->zoom == 0)
+	mlx_string_put(param->mlx, param->win, 200, 10, 0x00FFFF, "A partir de la le zoom va retourner la piece");
+      set_coord(param->coord, param->i, xd, yd, param->zoom);
+      draw_with_tab(param->coord, param->i, param->maxline, param);
+      mlx_key_hook(param->win, close_mlx, param);
+      //  x+= 40;
+      //fill_iso_rect(param->mlx, param->win, x, y, 20,0x00FFFF);
     }
   if(keycode == 1)
     {
-      y += 40;
-      fill_iso_rect(param->mlx, param->win, x, y,20,0x00FFFF);
+      yd += 40;
+      mlx_destroy_window(param->mlx, param->win);
+      param->win = mlx_new_window(param->mlx, 900, 900, "42");
+      legend(param);
+      if(param->zoom == 0)
+	mlx_string_put(param->mlx, param->win, 200, 10, 0x00FFFF, "A partir de la le zoom va retourner la piece");
+      set_coord(param->coord, param->i, xd, yd, param->zoom);
+      draw_with_tab(param->coord, param->i, param->maxline, param);
+      mlx_key_hook(param->win, close_mlx, param);
+      //y += 40;
+      //fill_iso_rect(param->mlx, param->win, x, y,20,0x00FFFF);
     }
   if(keycode == 13)
     {
-      y -= 40;
-      fill_iso_rect(param->mlx, param->win, x, y,20,0x00FFFF);
+      yd -= 40;
+      mlx_destroy_window(param->mlx, param->win);
+      param->win = mlx_new_window(param->mlx, 900, 900, "42");
+      legend(param);
+      if(param->zoom == 0)
+	mlx_string_put(param->mlx, param->win, 200, 10, 0x00FFFF, "A partir de la le zoom va retourner la piece");
+      set_coord(param->coord, param->i, xd, yd, param->zoom);
+      draw_with_tab(param->coord, param->i, param->maxline, param);
+      mlx_key_hook(param->win, close_mlx, param);
+      //y -= 40;
+      //fill_iso_rect(param->mlx, param->win, x, y,20,0x00FFFF);
     }
   if(keycode == 0)
     {
-      x -= 40;
-      fill_iso_rect(param->mlx, param->win, x, y,20,0x00FFFF);
+      xd -= 40;
+      mlx_destroy_window(param->mlx, param->win);
+      param->win = mlx_new_window(param->mlx, 900, 900, "42");
+      legend(param);
+      if(param->zoom == 0)
+	mlx_string_put(param->mlx, param->win, 200, 10, 0x00FFFF, "A partir de la le zoom va retourner la piece");
+      set_coord(param->coord, param->i, xd, yd, param->zoom);
+      draw_with_tab(param->coord, param->i, param->maxline, param);
+      mlx_key_hook(param->win, close_mlx, param);
+      
+      // x -= 40;
+      //fill_iso_rect(param->mlx, param->win, x, y,20,0x00FFFF);
       //  mlx_cursor_move(param->mlx, param->win, x, y);
     }
   if(keycode == 12)
     {
+      param->zoom += 5;
       mlx_destroy_window(param->mlx, param->win);
-      //      param->win = mlx_new_window(param->mlx, 900, 900, "42");      
-
+      param->win = mlx_new_window(param->mlx, 900, 900, "42");
+      legend(param);
+      if(param->zoom == 0)
+	mlx_string_put(param->mlx, param->win, 200, 10, 0x00FFFF, "A partir de la le zoom va retourner la piece");
+      set_coord(param->coord, param->i, xd, yd, param->zoom);
+      draw_with_tab(param->coord, param->i, param->maxline, param);
+      mlx_key_hook(param->win, close_mlx, param);
+    }
+  if(keycode == 14)
+    {
+      param->zoom -= 5;
+      mlx_destroy_window(param->mlx, param->win);
+      param->win = mlx_new_window(param->mlx, 900, 900, "42");
+      legend(param);
+      if(param->zoom == 0)
+	mlx_string_put(param->mlx, param->win, 200, 10, 0x00FFFF, "A partir de la le zoom va retourner la piece");
+      set_coord(param->coord, param->i, xd, yd, param->zoom);
+      draw_with_tab(param->coord, param->i, param->maxline, param);
+      mlx_key_hook(param->win, close_mlx, param);
+    }
+  if(keycode == 123)
+    {
+      //gauche
+      rotate -= 5;
+      mlx_destroy_window(param->mlx, param->win);
+      param->win = mlx_new_window(param->mlx, 900, 900, "42");
+      legend(param);
+      if(param->zoom == 0)
+	mlx_string_put(param->mlx, param->win, 200, 10, 0x00FFFF, "A partir de la le zoom va retourner la piece");
+      set_coord3(param->coord, param->i, xd, yd, param->zoom,rotate);
+      draw_with_tab(param->coord, param->i, param->maxline, param);
+      mlx_key_hook(param->win, close_mlx, param);
+    }
+  if(keycode == 124)
+    {
+      //      droite
+      rotate += 5;
+      mlx_destroy_window(param->mlx, param->win);
+      param->win = mlx_new_window(param->mlx, 900, 900, "42");
+      legend(param);
+      if(param->zoom == 0)
+	mlx_string_put(param->mlx, param->win, 200, 10, 0x00FFFF, "A partir de la le zoom va retourner la piece");
+      set_coord3(param->coord, param->i, xd, yd, param->zoom,rotate);
+      draw_with_tab(param->coord, param->i, param->maxline, param);
+      mlx_key_hook(param->win, close_mlx, param);
+    }
+  if(keycode == 15)
+    {
+  
+      mlx_destroy_window(param->mlx, param->win);
+      param->win = mlx_new_window(param->mlx, 900, 900, "42");
+      legend(param);
+      if(param->zoom == 0)
+	mlx_string_put(param->mlx, param->win, 200, 10, 0x00FFFF, "A partir de la le zoom va retourner la piece");
+      if(r == 0)
+	{
+	  r++;
+	  set_coord2(param->coord, param->i, xd, yd, param->zoom);
+	}
+      else if(r == 1)
+	{
+	  r = 0;
+	  set_coord(param->coord, param->i, xd, yd, param->zoom);
+	}
+      draw_with_tab(param->coord, param->i, param->maxline, param);
+      mlx_key_hook(param->win, close_mlx, param);
     }
   return (0);
 }
@@ -141,7 +270,6 @@ char **create_tab(char *argv, char **tab)
 
   fd = 0;
   ret = 0;
-  
   fd = open(argv, O_RDONLY);
   line = (char *)malloc(sizeof(char) * 2);
   while((ret = get_next_line(fd,&yt)))
@@ -156,7 +284,7 @@ char **create_tab(char *argv, char **tab)
 char ***create_coord_tab(char **tab, char ***coord, int i)
 {
   int index;
-
+  
   index = 0;
   coord = (char ***)malloc(sizeof(char **) * i + 1);
   while(index < i)
@@ -175,11 +303,9 @@ char ***create_coord_tab(char **tab, char ***coord, int i)
   return (coord);
 }
 
-char ***set_coord(char ***coord, int i)
+char ***set_coord3(char ***coord, int i, int x, int y, int zoom, int rotate)
 {
   int index;
-  int x;
-  int y;
   int xdep;
   int ydep;
   int xtemp;
@@ -190,8 +316,6 @@ char ***set_coord(char ***coord, int i)
   v = 0;
   j = 0;
   index = 0;
-  x = 300;
-  y = 60;
   xdep = x;
   ydep = y;
   while(index < i)
@@ -211,22 +335,132 @@ char ***set_coord(char ***coord, int i)
 	{
 	  x = xdep;
 	  y = ydep;
-	  x -= 20;
-	  y += 20;
+	  //	  x -= zoom - rotate;//a achanger
+	  y += zoom;
 	  xdep = x;
 	  ydep = y;
 	}
       else
 	{
-	  x += 20;
-	  y += 20;
+	  x += zoom;
+	  //y += zoom - rotate;// a changer
 	  if(coord[index][2][0] != '0')
 	    {
 	      v = ft_atoi(coord[index][2]);
 	      xtemp = x + v;
-	      ytemp = y - ((v * 2) + 1);
+	      ytemp =  y - ((v * 2)  + 1);
 	      j++;
 	    }
+	}
+      index++;
+    }
+  return (coord);
+}
+
+char ***set_coord2(char ***coord, int i, int x, int y, int zoom)
+{
+  int index;
+  int xdep;
+  int ydep;
+  int xtemp;
+  int ytemp;
+  int j;
+  int v;
+
+  v = 0;
+  j = 0;
+  index = 0;
+  xdep = x;
+  ydep = y;
+  while(index < i)
+    {
+      if(j == 0)
+	{
+	  coord[index][0] = ft_itoa(x);
+	  coord[index][1] = ft_itoa(y);
+	}
+      else
+	{
+	  coord[index][0] = ft_itoa(xtemp);
+	  coord[index][1] = ft_itoa(ytemp);
+	  j = 0;
+	}
+      if(coord[index][2][0] == '\n')
+	{
+	  x = xdep;
+	  y = ydep;
+	   x -= zoom;
+	  y += zoom;
+	  xdep = x;
+	  ydep = y;
+	}
+      else
+	{
+	  x += zoom;
+		  y += zoom;
+	  if(coord[index][2][0] != '0')
+	    {
+	      v = ft_atoi(coord[index][2]);
+	      if(v > 0)
+		v = -v;
+	      xtemp = x + v;
+	      ytemp =  y - ((v * 2)  + 1);;
+	      j++;
+	    }
+	}
+      index++;
+    }
+  return (coord);
+}
+
+char ***set_coord(char ***coord, int i, int x, int y, int zoom)
+{
+  int index;
+  int xdep;
+  int ydep;
+  int xtemp;
+  int ytemp;
+  int j;
+  int v;
+
+  v = 0;
+  j = 0;
+  index = 0;
+  xdep = x;
+  ydep = y;
+  while(index < i)
+    {
+      if(coord[index][2][0] == '\n')
+	{
+	  x = xdep;
+	  y = ydep;
+	  x -= zoom;
+	  y += zoom;
+	  xdep = x;
+	  ydep = y;
+	}
+      else
+	{
+	  x += zoom;
+	  y += zoom;
+	  if(coord[index][2][0] != '0')
+	    {
+	      v = ft_atoi(coord[index][2]);
+	      xtemp = x + v;
+	      ytemp = y - ((v * 2)  + 1);
+	      j++;
+	    }
+	}
+      if(j == 0)
+	{
+	  coord[index][0] = ft_itoa(x);
+	  coord[index][1] = ft_itoa(y);
+	}
+      else
+	{
+	  coord[index][0] = ft_itoa(xtemp);
+	  coord[index][1] = ft_itoa(ytemp);
+	  j = 0;
 	}
       index++;
     }
@@ -240,7 +474,12 @@ void draw_with_tab(char ***coord, int i, int maxline, t_struct *param)
   int y1;
   int y2;
   int index;
+  int color;
+  int val1;
+  int futurindex;
 
+  futurindex = 0;
+  color = 0x00FFFF;
   index = 0;
   x1 = 0;
   x2 = 0;
@@ -254,10 +493,26 @@ void draw_with_tab(char ***coord, int i, int maxline, t_struct *param)
 	  y1 = ft_atoi(coord[index][1]);
 	  x2 = ft_atoi(coord[index + 1][0]);
 	  y2 = ft_atoi(coord[index + 1][1]);
-	  if(coord[index][2][0] != '\n')
+	  val1 = ft_atoi(coord[index][2]);
+	  /* if(val1 != 0)
 	    {
-	      draw_line2(param->mlx, param->win, x1, y1, x2, y2, 0x00FFFF);
+	      color = 0xFFFFFF;
 	    }
+	  else
+	  color = 0x00FFFF;*/
+	  /// if(coord[index][2][0] == '\n')
+	  // // {
+	    //   mlx_string_put(param->mlx, param->win, x1 - 10, y1 - 10, 0xFFFFFF, "salut");
+	  // }
+	  //	  mlx_string_put(param->mlx, param->win, x1 - 10, y1 - 10, 0xFFFFFF, coord[index][2]);
+	  //mlx_string_put(param->mlx, param->win, x1 - 10, y1 - 10, 0xFFFFFF, coord[index][2]);
+  //      mlx_string_put(param->mlx, param->win, x2 - 10, y2 - 10, 0xFFFFFF, coord[index][2]);
+	  if(coord[index + 1][2][0] != '\n')
+	    {
+	      draw_line2(param->mlx, param->win, x1, y1, x2, y2, color);
+	    }
+	  else
+	    futurindex++;
 	}
       if(index + maxline + 1 < i)
 	{
@@ -265,202 +520,109 @@ void draw_with_tab(char ***coord, int i, int maxline, t_struct *param)
 	  y1 = ft_atoi(coord[index][1]);
 	  x2 = ft_atoi(coord[index + maxline + 1][0]);
 	  y2 = ft_atoi(coord[index + maxline + 1][1]);
-	  draw_line2(param->mlx, param->win, x1, y1, x2, y2, 0x00FFFF);
+	  if(coord[index + maxline + 1][2][0] != '\n')
+	    {
+	      draw_line2(param->mlx, param->win, x1, y1, x2, y2, color);	
+	    }
 	}
-      index++;
+      if(futurindex > 0)
+	{
+	  index += 2;
+	  futurindex = 0;
+	}
+      else
+	index++;
     }
 }
 
+
+int dothis()
+{
+  ft_putstr("lol");
+  return (0);
+}
 
 int main(int argc, char **argv)
 { 
   void *mlx;
   void *win;
-  int fd;
   char *line;
-  int ret;
-  char *yt;
-  char **tab;
   int i;
-  int index;
-  char ***coord;
-  int v;
-  int j;
-  int x;
-  int y;
-  int xdep;
-  int ydep;
-  int xtemp;
-  int ytemp;
-  int x1;
-  int y1;
-  int x2;
-  int y2;
   int maxline;
-  int color;
-  int val;
-  int val2;
   t_struct *mystruct;
-  void *try_img; 
-  MLXImage *img;
-  t_img *myimg;  
+  char ***coord;
+  char **tab;
+  int index;
 
-  //void    *mlx_new_image(void *mlx_ptr,int width,int height);
- /*
-**  return void *0 if failed
-*/
-  //  char    *mlx_get_data_addr(void *img_ptr, int *bits_per_pixel,
-  //			     int *size_line, int *endian);
-  /*
-  **  endian : 0 = sever X is little endian, 1 = big endian
-  **  endian : useless on macos, client and graphical framework have the same endian
-  */
-  //  int     mlx_put_image_to_window(void *mlx_ptr, void *win_ptr, void *img_ptr,
-  //				  int x, int y);
-  //unsigned int    mlx_get_color_value(void *mlx_ptr, int color);
-  
-  color = 0x00FFFF;
-  val2 = 0;
-  val = 0;
-  maxline = 0;
-  x = 300;
-  y = 60;
-  xdep = x;
-  ydep = y;
-  j = 0;
-  v = 0;
-  i = 0;
   index = 0;
-  mlx = mlx_init();
-
+  maxline = 0;
+  i = 0;
+  
 //GERER ERREUR PARAM  
   if(argc != 2)
     {
       printf("Bug arg");
       return (0);
     }
-  /*
-  fd = open(argv[1], O_RDONLY);
-  line = (char *)malloc(sizeof(char) * 2);
-  while((ret = get_next_line(fd,&yt)))
-    {
-      line = ft_strjoins(line,yt);
-      line = ft_strjoins(line, " \n ");
-    }
-    tab = ft_strsplit(line, ' ');*/
+
+////////////Init mlx//
+  mlx = mlx_init();
+  ///////////////
+  
+  /////////Create win//////// 
+  win = mlx_new_window(mlx, 900, 900, "42");
+  ////////////\\\\\\\\\\\\\\\\\\\\\
+  
+  //Create struct////
+ mystruct = malloc(sizeof(mystruct) * 6);
+  mystruct->mlx = mlx;
+  mystruct->win = win;
+  mystruct->zoom = 20;
+  legend(mystruct);
+  /////////////////
   
   //creation du tab////
   tab = create_tab(argv[1], tab);
+  /////////////////
+
+  /////////Take my max range tab///////
   while(tab[i])
     i++;
+  /////////////////
+  
+  ////////set coord and mallloc||||||||||||
   coord = create_coord_tab(tab, coord, i);
-    /* coord = (char ***)malloc(sizeof(char **) * i + 1);
-  while(index < i)
-    {
-      coord[index] = (char **)malloc(sizeof(char *) * 5);
-      index++;
-    }
-  index = 0;
-  while(index < i)
-    {
-      coord[index][0] = (char *)malloc(sizeof(char) * 5); 
-      coord[index][1] = (char *)malloc(sizeof(char) * 5);
-      coord[index][2] = tab[index];
-      index++;
-      }*//*
-  index = 0;
-  while(index < i)
-    {
-      if(j == 0)
-	{
-	  coord[index][0] = ft_itoa(x);
-	  coord[index][1] = ft_itoa(y);
-	}
-      else
-	{
-	  coord[index][0] = ft_itoa(xtemp);
-	  coord[index][1] = ft_itoa(ytemp);
-	  j = 0;
-	}
-      if(coord[index][2][0] == '\n')
-	{
-	  x = xdep;
-	  y = ydep;
-	  x -= 20;
-	  y += 20;
-	  xdep = x;
-	  ydep = y;
-	}
-      else
-	{
-	  x += 20;
-	  y += 20;
-	  if(coord[index][2][0] != '0')
-	    {
-	      v = ft_atoi(coord[index][2]);
-	      xtemp = x + v;
-	      ytemp = y - ((v * 2) + 1);
-	      j++;
-	    }
-	}
-      index++;
-      }*/
-  coord = set_coord(coord, i);
-  index = 0;  
-  win = mlx_new_window(mlx, 900, 900, "42");
-//img/////////////////////img//////////
-  /*try_img =  mlx_new_image(mlx, 200, 200);  
- myimg =  mlx_new_image(mlx, 200, 200);
- mlx_string_put(mlx, win, 0, 0, 0x00FFFF, "bonjour HIII");
-myimg->bbp =  mlx_get_color_value(mlx, 0x00FFFF);
- myimg->size_line = 4;
- mlx_put_image_to_window(mlx, win, myimg, 0, 0);*/
- //////////////////
- mystruct = malloc(sizeof(mystruct) * 2);
-  mystruct->mlx = mlx;
-  mystruct->win = win;
+  coord = set_coord(coord, i, 300, 60, mystruct->zoom);
+  ////////////////////////////////
+  
+  /////MY maxline//////
   while(tab[index][0] != '\n')
     {
       if(tab[index][0] != ' ')
 	maxline++;
       index++;
     }
-  index = 0;
-  draw_with_tab(coord, i, maxline, mystruct);
-  //Tracer une ligne avec 2 point
-    /* while(index + 1 < i)
-    {
-      if(index + 1 < i)
-	{
-	  x1 = ft_atoi(coord[index][0]);
-	  y1 = ft_atoi(coord[index][1]);
-	  x2 = ft_atoi(coord[index + 1][0]);
-	  y2 = ft_atoi(coord[index + 1][1]);
-	  val = ft_atoi(coord[index][2]);	  
-	  val2 = ft_atoi(coord[index + 1][2]);
-	  //pour ne pas relier des mauvais point
-	  if(coord[index][2][0] != '\n')
-	    {
-	      draw_line2(mlx, win, x1, y1, x2, y2, color);
-	    }
-	}
-      if(index + maxline + 1 < i)
-	{
-	  x1 = ft_atoi(coord[index][0]);
-	  y1 = ft_atoi(coord[index][1]);
-	  x2 = ft_atoi(coord[index + maxline + 1][0]);
-	  y2 = ft_atoi(coord[index + maxline + 1][1]);
-	  val = ft_atoi(coord[index][2]);
-	  val2 = ft_atoi(coord[index + maxline + 1][2]);
+  //////////////////////
 
-	  draw_line2(mlx, win, x1, y1, x2, y2, color);
-	 }
-      index++;
-      }*/
+  mystruct->i = i;
+  mystruct->maxline = maxline;
+  mystruct->coord = coord;
+  
+  ////Draw with my tab
+  draw_with_tab(coord, i, maxline, mystruct);
+  ////////////////////////////
+  
+  ///////Function appeler quand on fait un truc a la fenetre, faut le remettre dans le new fen//////////////////////
+  // mlx_expose_hook (mystruct->win, dothis, 0);  
+  ////////////////////////
+  
   //Fonction pour utiliser le clavier
-  mlx_key_hook(win, close_mlx, mystruct);
+  mlx_key_hook(mystruct->win, close_mlx, mystruct);
+  ///////////////////////////
+  
   //Boucle pour tenir la fenetre
   mlx_loop(mlx);
+  ///////////////////
   return(0);
 
 }
